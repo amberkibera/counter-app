7476850d630570a6ebb6208142a947bac696e5ad
amberkibera <awk5702@psu.edu>
2025-02-20 13:30:20 -0500

    Changes

--- counter-app.js
+++ counter-app.js
@@ -20,6 +20,7 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
+    this.count = 0;
     this.title = "";
     this.t = this.t || {};
     this.t = {
@@ -54,7 +55,7 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
       }
-      :host([count="10"]){
+      :host([count="18"]){
         color: var(--ddd-theme-default-athertonViolet);
       }
       .wrapper {
@@ -67,17 +68,58 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
     `];
   }
 
+  updated(changedProperties) {
+    super.updated(changedProperties);
+    if (changedProperties.has("count")) {
+      console.log("count changed to: ", this.count);
+      if (this.count == 21) {
+       this.makeItRain();
+      }
+    }
+  }
+
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
+
   // Lit render the HTML
   render() {
     return html`
-    <div class="wrapper">
-    <div class ="counter">${this.count}</div>
-    <div class ="buttons">
-      <button @click="${this.decrease}">-1</button>
-      <button @click="${this.increase}">+1</button>
+    <confetti-container id="confetti">
+
+      <div class="wrapper">
+        <div class ="counter">${this.count}</div>
+        <div class ="buttons">
+          <button @click="${this.decrease}" ?disabled="${this.count === this.min}">-1</button>
+          <button @click="${this.increase}" ?disabled="${this.isMax()}">+1</button>
+        </div>
       </div>
-    </div>
+    </confetti-container>
     ` ;
+  }
+
+  isMax() {
+    return this.count == this.max;
+  }
+
+  increase() {
+    this.count++;
   }
 
   /**
